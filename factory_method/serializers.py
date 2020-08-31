927521ca61ef676a6a0f1b47ab7cab135e2e7f28
vdyminskyy <v.dyminskyy@gmail.com>
2020-08-31 12:14:01 +0300

    add factory_method

---
+++ factory_method/serializers.py
@@ -0,0 +1,59 @@
+import json
+import xml.etree.ElementTree as et
+
+
+class SerializerFactory:
+    def __init__(self):
+        self._creators = {}
+
+    def register_format(self, format_, creator):
+        self._creators[format_] = creator
+
+    def get_serializer(self, format_):
+        creator = self._creators.get(format_)
+        if not creator:
+            raise ValueError(format_)
+        return creator()
+
+
+class JsonSerializer:
+    def __init__(self):
+        self._current_object = None
+
+    def start_object(self, object_name, object_id):
+        self._current_object = {
+            'id': object_id,
+        }
+
+    def add_property(self, name, value):
+        self._current_object[name] = value
+
+    def to_str(self):
+        return json.dumps(self._current_object)
+
+
+class XmlSerializer:
+    def __init__(self):
+        self._element = None
+
+    def start_object(self, object_name, object_id):
+        self._element = et.Element(object_name, attrib={'id': object_id})
+
+    def add_property(self, name, value):
+        prop = et.SubElement(self._element, name)
+        prop.text = value
+
+    def to_str(self):
+        return et.tostring(self._element, encoding='unicode')
+
+
+factory = SerializerFactory()
+factory.register_format('JSON', JsonSerializer)
+factory.register_format('XML', XmlSerializer)
+
+
+class ObjectSerializer:
+    def serialize(self, serializable, format_):
+        serializer = factory.get_serializer(format_)
+        serializable.serialize(serializer)
+        return serializer.to_str()
---
+++ factory_method/yaml_serializer.py
@@ -0,0 +1,11 @@
+import yaml
+
+from factory_method import serializers
+
+
+class YamlSerializer(serializers.JsonSerializer):
+    def to_str(self):
+        return yaml.dump(self._current_object)
+
+
+serializers.factory.register_format('YAML', YamlSerializer)
